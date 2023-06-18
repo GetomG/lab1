7a76ddaeddecd9ab95777102763caea337eb373e
Suebsakul Ampa <kul1977@gmail.com>
2023-06-18 21:27:50 +0700

    initial

---
+++ lab_01/list_keyword.py
@@ -0,0 +1,7 @@
+# Python program to demonstrate the application of iskeyword()  
+# importing keyword library which has lists  
+import keyword  
+    
+# displaying the complete list using "kwlist()."  
+print("The set of keywords in this version is: ")  
+print( keyword.kwlist )
