bedd0150c0ccd5ed36b3354c8bd894c29eda154f
Suebsakul Ampa <kul1977@gmail.com>
2023-06-19 13:46:23 +0700

    save variable

---
+++ lab_01/variable_list.py
@@ -0,0 +1,23 @@
+list1  = [1, "hi", "Python", 2]    
+#Checking type of given list  
+print(type(list1))  
+  
+#Printing the list1  
+print (list1)  
+  
+# List slicing  
+print (list1[3:])  
+  
+# List slicing  
+print (list1[0:2])   
+  
+# List Concatenation using + operator  
+print (list1 + list1)  
+  
+# List repetation using * operator  
+print (list1 * 3)  
+
+
+list1[0] = list1[0] * 8
+#Printing the list1  
+print (list1)  
---
+++ lab_01/variable_string.py
@@ -0,0 +1,22 @@
+str1 = 'hello javatpoint' #string str1    
+str2 = ' how are you' #string str2    
+print (str1[0:2]) #printing first two character using slice operator    
+print (str1[4]) #printing 4th character of the string    
+print (str1*2) #printing the string twice    
+print (str1 + str2) #printing the concatenation of str1 and str2    
+
+
+
+print (str2) #printing the concatenation of str2    
+print ("[{}]".format(str2)) #printing the concatenation of str2    
+
+print ("[{}]".format((str2.strip()))) #printing the concatenation of str2 and call method strip for trim space " "
+
+
+
+s = '''''A multiline 
+string'''  
+
+print("--------------------------------------------------------------------------------------------")
+print(s)  
+print("--------------------------------------------------------------------------------------------")
