6b39d24a11bf92aa3c0d6beac7248848343672fc
Suebsakul Ampa <kul1977@gmail.com>
2023-07-04 09:19:24 +0700

    add statement

--- lab_01/variable_set.py
+++ lab_01/variable_set.py
@@ -14,3 +14,33 @@ print(set2)
 #Removing element from the set  
 set2.remove(2)  
 print(set2)  
+
+import time
+import random
+import string
+my_set = {4, 1, 7, 2, 5}  # Example set
+
+print(my_set)
+
+for i in range(0, 10) :
+    # new_element = random.randint( 0, 512 )
+    my_set.add(random.randint( 0, 512 ))
+
+    # if my_set.add(new_element) is None:
+    #     print("Add new_element : {} done".format(new_element))
+    # else:
+    #     print("Add new_element : {} found duplicate".format(new_element))    
+    # time.sleep(1)
+
+# letters = string.ascii_lowercase
+# print ( ''.join(random.choice(letters) for i in range(10)) )
+
+for i in range(0, 10) :
+    letters = string.ascii_lowercase
+    my_set.add(''.join(random.choice(letters) for i in range(10)))    
+    print(my_set)
+    
+
+while True :
+    print(my_set.pop())
+    time.sleep(1)
